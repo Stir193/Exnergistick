59084db8574a60fe6713404dd27225d7cd157fa2
Stir193 <rtickto@gmail.com>
2025-10-18 09:14:23 -0500

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("invertirBtn");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Gracias por confiar en Exnergistick. Pronto tendrás noticias de tu inversión.");
+    });
+  }
+});
